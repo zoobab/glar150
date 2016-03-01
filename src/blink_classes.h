57dedd9196ab8199319fa8000b62c19b65f50d3b
Pieter Hintjens <ph@imatix.com>
2016-03-01 16:04:41 +0100

    Problem: lack files for building
    Solution: add to repository

--- src/blink_classes.h
+++ src/blink_classes.h
@@ -2,7 +2,7 @@
     blink_classes - private header file
 
     Copyright (c) the Contributors as noted in the AUTHORS file.       
-    This file is part of the Malamute Project.                         
+    This file is part of the Blink Project.                            
                                                                        
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
