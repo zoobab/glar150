75defc1c7aa442c5c02a80b45601a93bf9d117cc
Pieter Hintjens <ph@imatix.com>
2016-03-07 13:35:19 +0200

    Problem: daemon split into 3 files
    Solution: make a single file and stop/start glard along with WiFi

--- monitorgw/daemon.sh
+++ monitorgw/daemon.sh
@@ -1,2 +1,22 @@
-#!/bin/sh
-while true; do /root/monitorgw/check_ping.sh ; sleep 4; done &
+#! /bin/sh
+
+GLARD_UP=0
+while true; do
+    #   If wireless network is up, start glard
+    GW_IP="192.168.1.1"
+    ping -q -W1 -c2 ${GW_IP} > /dev/null
+    if [ $? -eq 0 -a $GLARD_UP -eq 0 ]; then
+        GLARD_UP=1
+        screen -d -L -m /usr/bin/glard -v
+    else
+        #   Stop glard if necessary
+        if [ $GLARD_UP -eq 1 ]; then
+            kill -9 `pgrep glard`
+            GLARD_UP=0
+            #   flash red LED here (error)...
+        else
+            #   flash green LED here (waiting)...
+        fi
+    fi
+    sleep 2
+done
