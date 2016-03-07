9058e99508eea14fa4df8b34a41331fe2edf4d97
Pieter Hintjens <ph@imatix.com>
2016-03-07 15:08:11 +0100

    Problem: if/then else logic error
    Solution: fixed it

--- monitorgw/daemon.sh
+++ monitorgw/daemon.sh
@@ -6,9 +6,11 @@ GW_IP="192.168.43.1"
 while true; do
     #   If wireless network is up, start glard
     ping -q -W1 -c2 ${GW_IP} > /dev/null
-    if [ $? -eq 0 -a $GLARD_UP -eq 0 ]; then
-        GLARD_UP=1
-        screen -d -L -m /usr/bin/glard -v
+    if [ $? -eq 0 ]; then
+        if [ $GLARD_UP -eq 0 ]; then
+            GLARD_UP=1
+            screen -d -L -m /usr/bin/glard -v
+        fi
     else
         #   Stop glard if necessary
         if [ $GLARD_UP -eq 1 ]; then
