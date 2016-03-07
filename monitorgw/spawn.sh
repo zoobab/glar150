cdda2e142764a5eb7bc2755502b01ef1e6173a8c
Benjamin Henrion <zoobab@gmail.com>
2016-03-07 12:17:27 +0100

    spawn.sh: s/blink_shell/glard

--- monitorgw/spawn.sh
+++ monitorgw/spawn.sh
@@ -6,7 +6,7 @@ while true; do
     if [ -f "${PINGFILE}" ]; then
     	grep -q "ok" ${PINGFILE}
     	if [ $? -eq 0 ]; then
-		screen -d -L -m /usr/bin/blink_shell -i wlan0 -v
+		screen -d -L -m /usr/bin/glard -i wlan0 -v
 		exit 0
     	else
 		sleep 3
